31ae36ee1d22a9d95008a00b40b403469c89938f
Bardark <ren_rbk@hotmail.es>
2017-08-25 14:43:25 -0500

    archivos en php y separacion de menu

---
+++ admin/menu.php
@@ -0,0 +1,208 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+  <a class="navbar-brand" href="#">Start Bootstrap</a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav navbar-sidenav">
+      <li class="nav-item active" data-toggle="tooltip" data-placement="right" title="Dashboard">
+        <a class="nav-link" href="#">
+          <i class="fa fa-fw fa-dashboard"></i>
+          <span class="nav-link-text">
+            Adminsitrador</span>
+        </a>
+      </li>
+
+      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+        <a class="nav-link" href="#">
+          <i class="fa fa-fw fa-table"></i>
+          <span class="nav-link-text">
+            Sesiones</span>
+        </a>
+      </li>
+
+      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Motores">
+        <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMotor">
+          <i class="fa fa-fw fa-sitemap"></i>
+          <span class="nav-link-text">
+            Motores</span>
+        </a>
+        <ul class="sidenav-second-level collapse" id="collapseMotor">
+          <li>
+            <a href="#">Agregar marca</a>
+          </li>
+          <li>
+            <a href="#">Ver - Editar marca</a>
+          </li>
+
+        </ul>
+      </li>
+
+      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Modelos de motor">
+        <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseModelo">
+          <i class="fa fa-fw fa-sitemap"></i>
+          <span class="nav-link-text">
+            Modelos de motor</span>
+        </a>
+        <ul class="sidenav-second-level collapse" id="collapseModelo">
+          <li>
+            <a href="#">Agregar modelo</a>
+          </li>
+          <li>
+            <a href="#">Ver - Editar modelo</a>
+          </li>
+
+        </ul>
+      </li>
+
+
+      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Marcas de filtro">
+        <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMarcaF">
+          <i class="fa fa-fw fa-sitemap"></i>
+          <span class="nav-link-text">
+            Marcas de filtros</span>
+        </a>
+        <ul class="sidenav-second-level collapse" id="collapseMarcaF">
+          <li>
+            <a href="#">Agregar marca de filtro</a>
+          </li>
+          <li>
+            <a href="#">Ver - Editar marca de filtro</a>
+          </li>
+
+        </ul>
+      </li>
+
+
+      <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Filtro">
+        <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseFiltro">
+          <i class="fa fa-fw fa-sitemap"></i>
+          <span class="nav-link-text">
+            Filtros</span>
+        </a>
+        <ul class="sidenav-second-level collapse" id="collapseFiltro">
+          <li>
+            <a href="#">Agregar filtro</a>
+          </li>
+          <li>
+            <a href="#">Ver - Editar filtro</a>
+          </li>
+
+        </ul>
+      </li>
+
+    </ul>
+    <ul class="navbar-nav sidenav-toggler">
+      <li class="nav-item">
+        <a class="nav-link text-center" id="sidenavToggler">
+          <i class="fa fa-fw fa-angle-left"></i>
+        </a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle mr-lg-2" href="#" id="messagesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-fw fa-envelope"></i>
+          <span class="d-lg-none">Messages
+            <span class="badge badge-pill badge-primary">12 New</span>
+          </span>
+          <span class="new-indicator text-primary d-none d-lg-block">
+            <i class="fa fa-fw fa-circle"></i>
+            <span class="number">12</span>
+          </span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="messagesDropdown">
+          <h6 class="dropdown-header">New Messages:</h6>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">
+            <strong>David Miller</strong>
+            <span class="small float-right text-muted">11:21 AM</span>
+            <div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">
+            <strong>Jane Smith</strong>
+            <span class="small float-right text-muted">11:21 AM</span>
+            <div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">
+            <strong>John Doe</strong>
+            <span class="small float-right text-muted">11:21 AM</span>
+            <div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item small" href="#">
+            View all messages
+          </a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle mr-lg-2" href="#" id="alertsDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-fw fa-bell"></i>
+          <span class="d-lg-none">Alerts
+            <span class="badge badge-pill badge-warning">6 New</span>
+          </span>
+          <span class="new-indicator text-warning d-none d-lg-block">
+            <i class="fa fa-fw fa-circle"></i>
+            <span class="number">6</span>
+          </span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="alertsDropdown">
+          <h6 class="dropdown-header">New Alerts:</h6>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">
+            <span class="text-success">
+              <strong>
+                <i class="fa fa-long-arrow-up"></i>
+                Status Update</strong>
+            </span>
+            <span class="small float-right text-muted">11:21 AM</span>
+            <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">
+            <span class="text-danger">
+              <strong>
+                <i class="fa fa-long-arrow-down"></i>
+                Status Update</strong>
+            </span>
+            <span class="small float-right text-muted">11:21 AM</span>
+            <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">
+            <span class="text-success">
+              <strong>
+                <i class="fa fa-long-arrow-up"></i>
+                Status Update</strong>
+            </span>
+            <span class="small float-right text-muted">11:21 AM</span>
+            <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item small" href="#">
+            View all alerts
+          </a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <form class="form-inline my-2 my-lg-0 mr-lg-2">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+              <button class="btn btn-primary" type="button">
+                <i class="fa fa-search"></i>
+              </button>
+            </span>
+          </div>
+        </form>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+          <i class="fa fa-fw fa-sign-out"></i>
+          Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
